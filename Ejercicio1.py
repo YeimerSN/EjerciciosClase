102ce4fa8b2ef08d103a2b14b38b6e00ef68874d
YeimerSN <52164044+YeimerSN@users.noreply.github.com>
2020-06-23 13:06:38 -0500

    Update Ejercicio1.py

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -1,1 +1,10 @@
-
+"""
+lista corresponde a la lista para tomar el ultimo digito y valores es una lista
+nula la cual guardara el ultimo elemento de cada numero en la lista
+"""
+def numero(lista, valores):
+    for i in range(len(lista)):
+        valores.append(lista[i] % 10)
+    print(''.join(map(str, valores)))
+            
+numero([132,456,789, 000, 584, 9, 0, 5, 71871], [])
